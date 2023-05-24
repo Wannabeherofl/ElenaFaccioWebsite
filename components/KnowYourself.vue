43db3cf24e913dc73ea0c4b5dcafead92a64127c
Wannabeherofl <shilohnclemons@yahoo.com>
2023-05-24 10:44:13 -0500

    Update KnowYourself.vue

--- components/KnowYourself.vue
+++ components/KnowYourself.vue
@@ -1,12 +1,13 @@
 <script setup></script>
 
 <template>
+    <br>
     <div class="flex flex-row place-content-center mb-12">
         <div class="max-w-lg p-10 rounded-xl bg-[#9cf5bd] dark:bg-slate-900">
             <section>
                 <article>
                     <h2 class="Third-font max-w-md text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
-                        Perchè lo faccio?
+                        skills direct behaviour for a known situation, but when you get into unknown situation, uncertain situation, we lean on our attributes. identity
                     </h2>
                     <p class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                         Know your identity means: you and your team make clear the skills for known situations, and know your attributes in unknow situation. 
