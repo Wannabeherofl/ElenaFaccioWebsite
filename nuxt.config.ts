8260b749140a013bac3c22258777630e6fceac0e
Wannabeherofl <shilohnclemons@yahoo.com>
2023-05-24 10:11:43 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@ export default defineNuxtConfig({
     modules: ['@nuxt/content'],
     ssr: true, //server side rendered enabled //When true
     target: "static", // and static, nuxt generates a hybrid static site
-    import: { projects } from "./utils/projectsData",
+    app: {
+      head: {
+        link: [
+          { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+          { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+          { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=PT+Serif:wght@400;700&display=swap' }
+        ]
+      },
+    },
+    css: ['~/assets/css/main.css'],
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+    import { projects } from "./utils/projectsData",
 
     export default {
       mode: "universal",
@@ -20,22 +36,6 @@ export default defineNuxtConfig({
           return paths;
         }
       }
-    },
-    app: {
-      head: {
-        link: [
-          { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-          { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-          { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=PT+Serif:wght@400;700&display=swap' }
-        ]
-      },
-    },
-    css: ['~/assets/css/main.css'],
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
     },
   })
   
