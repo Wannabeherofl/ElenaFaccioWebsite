c97871356c8f36b20f5967fb4a457458716f1dda
Wannabeherofl <shilohnclemons@yahoo.com>
2023-05-25 11:33:05 -0500

    big fix to info1

--- components/Info1.vue
+++ components/Info1.vue
@@ -13,22 +13,28 @@
                         With those approaching digital    
                     </h2>
                     <p class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
-                        <li>With those who want to go online but don't know how to do it</li>
-                        <li>With those who are already on social media but can't reach their goals</li>
-                        <li>With those who think these changes take up too much time and energy to work</li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                            With those who want to go online but don't know how to do it
+                        </li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                            With those who are already on social media but can't reach their goals
+                        </li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                            With those who think these changes take up too much time and energy to work
+                        </li>
                         <br>
                     </p>
                     <h2 class="Third-font max-w-md text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
                         With those who want to do marketing
                     </h2>
                     <p class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
-                        <li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                             with those who would like to make a more structured or efficient marketing and communication plan
                         </li>
-                        <li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                             with those who want to add a dash of innovation in their processes
                         </li>
-                        <li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                             with those who want to develop their strategy themselves, but want support with small software tips, contacts with professionals for specific activities, or an outside perspective                        </li>
                         <br>
                         <h2 class="Third-font max-w-md text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
@@ -40,13 +46,13 @@
                         <h2 class="Third-font max-w-md text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
                             With those who want to get the most out of
                         </h2>
-                        <li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                             with those who would like to optimize the energy they are already investing in digital
                         </li>
-                        <li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                             with those who would like to expand the social media they are already using 
                         </li>
-                        <li>
+                        <li class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
                             with those who would like to improve the strategies they are already applying
                         </li>
                     </p>
