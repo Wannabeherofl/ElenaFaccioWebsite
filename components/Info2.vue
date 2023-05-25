6c3734b33cb420e7cd6399cdb0c78aa20900a80d
Wannabeherofl <shilohnclemons@yahoo.com>
2023-05-25 13:10:41 -0500

    Update Info2.vue

--- components/Info2.vue
+++ components/Info2.vue
@@ -6,56 +6,56 @@
         <div class="p-10 rounded-xl bg-[#9cf5bd] dark:bg-slate-900">
             <section>
                 <article>
-                    <h1 class="Third-fonttext-7xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
+                    <h1 class="Third-fonttext-7xl text-center  text-slate-900 dark:text-white">
                         How do I do it?
                     </h1>
                     <br>
-                    <h2 class="Third-font text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
+                    <h2 class="Third-font text-4xl text-center ext-slate-900 dark:text-white">
                         Innovating    
                     </h2>
-                    <p class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Business Identity communication strategies
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Business Strategies
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Designing Digitization Pathways
                         </li>
                         <br>
                     </p>
                     <br>
-                    <h2 class="Third-font text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
+                    <h2 class="Third-font text-4xl text-center text-slate-900 dark:text-white">
                         teaching
                     </h2>
-                    <p class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             social/digital education 
                         </li>
-                        <li class=" text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class=" text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Platform analysis
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Provide guidelines for the use of key marketing tools
                         </li>
                         <br>
-                        <h2 class="Third-font text-4xl text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
+                        <h2 class="Third-font text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
                             Marketing
                         </h2>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Marketing analysis and activities
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Communication strategy development
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Access to my network of professionals
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Tailored back-office services
                         </li>
-                        <li class="text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
                             Project manager for site, logo and product renewal
                         </li>
                     </p>
