659e680cf3f61d9d0b49e474da7e705b59e7c6e3
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-05-31 11:28:12 -0500

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,26 +1,37 @@
-  <script setup>
-  function myFunction(e) {
-        var curr = e.target.textContent;
-        var elem = document.querySelectorAll('#Clicker');
-        for (var i = 0; i < elem.length; i++) {
-            if (elem[i].textContent === curr) {
-                elem[i].className = 'active';
-            } else {
-                elem[i].className = '';
-            }
-        }
-    };
-  </script>
+<script lang="ts" setup>
+// create variable for mobile navigation
+const isMobileNavVisible = ref<boolean>(false);
+// method when the menu button is click
+const menuTrigger = (): void => {
+  if (isMobileNavVisible.value) {
+    isMobileNavVisible.value = false;
+    return;
+  }
+
+  isMobileNavVisible.value = true;
+};
+// useHead composable to add CSS class in the HTML body
+// when the menu trigger is click
+useHead({
+  bodyAttrs: {
+    class: computed(() => {
+      if (isMobileNavVisible.value) return 'menu-is-open';
+      
+      return '';
+    }),
+  },
+});
+</script>
 
   <template>
       <header class="text-center bg-white dark:bg-black text-black dark:text-white sticky top-0 z-10">
         <div class="flex mx-40 pt-16 pb-2">
-              <nav id="Clicker" class="active flex h-16 Second-Font sm:block space-x-8 text-2xl pr-6 pl-6" onclick="myFunction(e)" aria-label="main">
+              <nav id="Clicker" class="active flex h-16 Second-Font sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
               <NuxtLink class="link hover:opacity-90" to="/">
                 Home
               </NuxtLink>
               </nav>
-              <nav id="Clicker" class="Clicker flex h-16 Second-Font sm:block space-x-8 text-2xl pr-6 pl-6" onclick="myFunction(e)" aria-label="main">
+              <nav id="Clicker" class="flex h-16 Second-Font sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
                 <NuxtLink href="../AboutMe" class="h-16 w-16 link hover:opacity-90">
                   About Me
                 </NuxtLink>
@@ -35,12 +46,12 @@
               <button id="mobile-open-button" class="text-3xl sm:hidden focus:outline-none">
                 &#9776;
               </button>
-                <nav id="Clicker" class="Clicker h-16 Second-Font hidden sm:block space-x-8 text-2xl pr-6 pl-6" onclick="myFunction(e)" aria-label="main">
+                <nav id="Clicker" class="h-16 Second-Font hidden sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
                   <NuxtLink href="../services" class="link hover:opacity-90">
                   Services
                 </NuxtLink>
                 </nav>
-                <nav id="Clicker" class="Clicker h-16 Second-Font hidden sm:block space-x-8 text-2xl pr-6 pl-6" onclick="myFunction(e)" aria-label="main">
+                <nav id="Clicker" class="h-16 Second-Font hidden sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
                   <a href="../Contact.html" class="h-16">
                     Contact
                   </a>
