4ab3de1246763839cb5b87843e13202df62cdedd
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-05-31 09:55:22 -0500

    Update Elements.vue

--- components/Elements.vue
+++ components/Elements.vue
@@ -11,50 +11,39 @@
         <ul class="list-none mx-auto mt-12 flex flex-col sm:flex-row items-center gap-8">
           <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-slate-900 py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/F.png" alt="Camilla">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Camilla</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">Cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">Cat</p>
+            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Focused</h3>
           </li>
           <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-slate-900 py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/Ac.png" alt="Zelda">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Zelda</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">also a cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">also a cat</p>
+            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Accountable</h3>
           </li>
           <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-slate-900 py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/C.png" alt="Camilla">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Me</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">possibly a cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">possibly a cat</p>
-          </li>
+            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Creative</h3>          </li>
           <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-slate-900 py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/I.png" alt="Camilla">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Me</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">possibly a cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">possibly a cat</p>
-          </li>
+            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Informed</h3>
+            </li>
           <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-slate-900 py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/O.png" alt="Camilla">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Me</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">possibly a cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">possibly a cat</p>
-          </li>
+            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Opened</h3>
+            </li>
         </ul>
         <div class="pt-5 flex items-center text-center gap-8 Second-Font text-2xl text-slate-700 dark:text-slate-400">
           <p class="w-1/2">
-            "Focused"
+            "I'm always very focused on the project and in doing my best pursuing firms goals"
           </p>
           <p class="w-1/2">
-            "Accountable"
+            "you will not feel like you are alone if im next to you"
           </p>
           <p class="w-1/2">
-            "Creative"
+            "Yes"
           </p>
           <p class="w-1/2">
-            "Informed"
+            "I study and read a lot, im a book lover and i am passionate to leanr and discover new ideas and know-hows"
           </p>
           <p class="w-1/2">
-            "Opened"
+            "One of my focuses are listen to people and be open. I manage on projects pursuing goals of effectiveness but always listening to people"
           </p>
         </div>
       </section>
