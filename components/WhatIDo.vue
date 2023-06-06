d33c557862a783ebdf3c2fe33461f79355949a3a
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-06 14:25:05 -0500

    Update WhatIDo.vue

--- components/WhatIDo.vue
+++ components/WhatIDo.vue
@@ -3,13 +3,13 @@
 <template>
     <img src="../assets/HeadImage2.jpg" alt="headimages" class="sm:mb-32 pt-16" /> 
     <div class="text-center mb-12">
-        <div class="p-10 rounded-xl bg-[#2C2C2C] dark:bg-[#2C2C2C]">
+        <div class="p-10 rounded-xl bg-[#242424] dark:bg-[#242424]">
         <section>
             <article>
                 <h2 class="Third-font text-7xl text-center  text-slate-900 dark:text-white">
                 Cosa Faccio
             </h2>
-            <p class="text-2xl mt-4 text-center text-slate-700 dark:text-slate-400">
+            <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
                 Aiuto le persone a rendere conscia la loro <strong class="text-orange-500">identità</strong> professionale e più efficaci le loro attività di marketing e comunicazione
             </p>
             </article>
