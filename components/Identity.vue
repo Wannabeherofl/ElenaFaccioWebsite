ed430357193b30d59a3bee9e7300e8e7ece63487
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-07 08:26:04 -0500

    font fix

--- components/Identity.vue
+++ components/Identity.vue
@@ -5,7 +5,7 @@
         <div class="p-10 rounded-xl bg-[#121212] dark:bg-[#121212]">
             <section>
                 <article>
-                <h2 class="Third-font text-7xl text-center  text-slate-900 dark:text-white">
+                <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
                 io credo che l'<strong class="text-orange-400">identità</strong> professionale sia la base per ogni idea business 
                 </h2>
                 </article>
