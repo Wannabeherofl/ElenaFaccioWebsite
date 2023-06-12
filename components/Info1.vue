383162d46f46d20130f46a14892fe5f64371c9f0
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-12 09:24:34 -0500

    infoupdate

--- components/Info1.vue
+++ components/Info1.vue
@@ -1,66 +1,53 @@
 <script setup></script>
 
 <template>
-    <br>
-    <div class="text-center mb-12">
-        <div class="p-10 rounded-xl bg-[#121212] dark:bg-[#121212]">
-            <section>
-                <article class="Second-Font">
-                    <h1 class="Third-font text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                        Who do I do it with?
-                    </h1>
-                    <br>
-                    <h2 class="Third-font text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                        With those approaching digital    
-                    </h2>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            With those who want to go online but don't know how to do it
-                        </li>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            With those who are already on social media but can't reach their goals
-                        </li>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            With those who think these changes take up too much time and energy to work
-                        </li>
-                        <br>
-                    </p>
-                    <br>
-                    <h2 class="Third-font text-7xl text-center text-slate-900 dark:text-white">
-                        With those who want to do marketing
-                    </h2>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            with those who would like to make a more structured or efficient marketing and communication plan
-                        </li>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            with those who want to add a dash of innovation in their processes
-                        </li>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            with those who want to develop their strategy themselves, but want support with small software tips, contacts with professionals for specific activities, or an outside perspective                        </li>
-                        <br>
-                        <h2 class="Third-font text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                            why call me for a project /<br>
-                            why make me your consultant /<br> 
-                            how can i help you with /<br> 
-                            which goal i can help you with<br>
-                        </h2>
-                        <br>
-                        <h2>
-                            With those who want to get the most out of
-                        </h2>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            with those who would like to optimize the energy they are already investing in digital
-                        </li>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            with those who would like to expand the social media they are already using 
-                        </li>
-                        <li class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            with those who would like to improve the strategies they are already applying
-                        </li>
-                    </p>
-                </article>
-            </section>
-        </div>
-    </div> 
-</template>
+    <main class="max-4-4xl mx-auto">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+        <div class="Main-font hidden sm:block text-7xl"></div>
+      </section>
+      <hr class="mx-auto bg-white dark:bg-black w-1/2">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+        <article>
+          <h1 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+            Who do I do it with?
+          </h1>
+          <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+            With those approaching digital
+          </h2>
+          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            <ul>
+              <li>With those who want to go online but don't know how to do it</li>
+              <li>With those who are already on social media but can't reach their goals</li>
+              <li>With those who think these changes take up too much time and energy to work</li>
+            </ul>
+          </p>
+          <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+            With those who want to do marketing
+          </h2>
+          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            <ul>
+              <li>with those who would like to make a more structured or efficient marketing and communication plan</li>
+              <li>with those who want to add a dash of innovation in their processes</li>
+              <li>with those who want to develop their strategy themselves, but want support with small software tips, contacts with professionals for specific activities, or an outside perspective</li>
+            </ul>
+          </p>
+          <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+            why call me for a project /<br>
+            why make me your consultant /<br> 
+            how can i help you with /<br> 
+            which goal i can help you with<br>
+          </h2>
+          <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+            With those who want to get the most out of
+          </h2>
+          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            <ul>
+              <li>with those who would like to optimize the energy they are already investing in digital</li>
+              <li>with those who would like to expand the social media they are already using</li>
+              <li>with those who would like to improve the strategies they are already applying</li>
+            </ul>
+          </p>
+        </article>
+      </section>
+    </main>
+  </template>
