75b71efb7dd6d935cb528a9e8672f024c88fb686
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-12 09:35:20 -0500

    Update WhyIDo.vue

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -1,28 +1,6 @@
 <script setup></script>
 
 <template>
-     <div class="text-center mb-12">
-        <div class="p-10 rounded-xl bg-[#121212] dark:bg-[#121212]">
-            <section>
-                <article class="Second-Font">
-                    <h2 class="Third-font text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                        Perchè lo faccio?
-                    </h2>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                        95% <strong class="text-[#5bd086]">delle scelte</strong> di consumo sono prese in modo inconscio
-                    </p>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                        97% <strong class="text-orange-500">delle persone</strong> che sono consce della propria identità: vive più a lungo e meglio
-                    </p>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                        il nostro inconscio prende una decisione <strong class="text-[#5bd086]">7</strong> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
-                    </p>
-                </article>
-            </section>
-        </div>
-    </div> 
-</template>
-<template>
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
         <div class="Main-font hidden sm:block text-7xl"></div>
