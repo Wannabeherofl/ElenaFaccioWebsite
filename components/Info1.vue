c8833d758bdad3ed233ddf626c6e0eb19203cd6d
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-12 09:43:26 -0500

    test

--- components/Info1.vue
+++ components/Info1.vue
@@ -16,9 +16,15 @@
           </h2>
           <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             <ul>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">With those who want to go online but don't know how to do it</li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">With those who are already on social media but can't reach their goals</li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">With those who think these changes take up too much time and energy to work</li>
+              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+                With those who want to go online but don't know how to do it
+            </li>
+              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+                With those who are already on social media but can't reach their goals
+            </li>
+              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+                With those who think these changes take up too much time and energy to work
+            </li>
             </ul>
           </p>
           <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
