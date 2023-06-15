c8d6108c6de9c21c8ac51e01c8647f26aa09afcc
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-15 16:07:31 -0500

    Update ContactInfo.vue

--- components/ContactInfo.vue
+++ components/ContactInfo.vue
@@ -3,7 +3,7 @@
 <template>
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
-        <div class="Main-font hidden sm:block text-7xl"></div>
+        <div class="Main-font hidden sm:block text-7xl">Contact Information</div>
       </section>
       <hr class="mx-auto bg-white dark:bg-black w-1/2">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
@@ -12,13 +12,14 @@
           <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             Email Address
           </h2>
-          <p class="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+          <p class="max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             <a href="mailto:elena_faccio@live.it">elena_faccio@live.it</a>
           </p>
+          <hr class="mx-auto bg-white dark:bg-black w-1/2">
           <h2 class="Third-font max-w-auto text-4xl font-bold text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
             Phone Number
           </h2>
-          <p class="max-w-md text-2xl mt-4 ml-14 text-center sm:text-left text-slate-700 dark:text-white">
+          <p class="max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             <a>+39 (346) 758-7566</a>
           </p>
         </article>
