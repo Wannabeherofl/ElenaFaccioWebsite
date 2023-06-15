37b8e06a6de7352faa0a04bbe617cd9601d5f117
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-15 15:38:31 -0500

    text size

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -8,19 +8,19 @@
           <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             Perchè lo faccio?
           </h2>
-          <p class="flex Third-font max-w-2xl text-2xl mt-4 text-center sm:text-left text-orange-500 dark:text-white">
+          <p class="flex Third-font max-w-2xl text-3xl mt-4 text-center sm:text-left text-orange-500 dark:text-white">
             <strong>95%</strong>
           </p>
-          <p class="Second-Font max-w-2xl text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             delle scelte di consumo sono prese in modo inconscio
           </p>
-          <p class="flex Third-font max-w-2xl text-2xl mt-4 text-center sm:text-left text-orange-500 dark:text-white">
+          <p class="flex Third-font max-w-2xl text-3xl mt-4 text-center sm:text-left text-orange-500 dark:text-white">
             <strong>97%</strong>
           </p>
-          <p class="Second-Font max-w-2xl text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             delle persone che sono consce della propria identità: vive più a lungo e meglio
           </p>
-          <p class="Second-Font max-w-2xl text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             il nostro inconscio prende una decisione <span class="Third-Font font-bold text-[#5bd086]">7</span> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
           </p>
         </article>
