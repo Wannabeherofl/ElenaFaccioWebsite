83e62b2f9fd3e6d56fc5a68f78106c1decc192ee
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-15 15:50:21 -0500

    contact

---
+++ components/ContactInfo.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+<template>
+    <main class="max-4-4xl mx-auto">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+        <div class="Main-font hidden sm:block text-7xl"></div>
+      </section>
+      <hr class="mx-auto bg-white dark:bg-black w-1/2">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+        <img src="../assets\IMG-20200704-WA00012.jpg" class="mb-6 h-72 rounded w-auto">
+        <article>
+          <h2 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+            Email Address
+          </h2>
+          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            <a href="mailto:elena_faccio@live.it">elena_faccio@live.it</a>
+          </p>
+        </article>
+      </section>
+      <hr class="mx-auto bg-white dark:bg-black w-1/2">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+        
+        <article>
+          <h2 class="Third-font max-w-auto text-4xl font-bold text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
+            Phone Number
+          </h2>
+          <p class="Second-Font max-w-md text-2xl mt-4 ml-14 text-center sm:text-left text-slate-700 dark:text-white">
+            <a>+39 (346) 758-7566</a>
+          </p>
+        </article>
+      </section>
+    </main>
+</template>
---
+++ pages/Contact.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="dark:bg-black">
+      <Header />
+      <Elements />
+      <Aboutmemain />
+    </div>
+  </template>
+  <script setup>
+  
+  </script>
