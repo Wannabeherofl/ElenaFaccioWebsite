5fd846c892ef2df4ddd4d4496e6222fb5d762621
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-16 09:49:20 -0500

    Update WhyIDo.vue

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -4,10 +4,11 @@
     <main class="max-4-4xl mx-auto">
       <hr class="mx-auto bg-white dark:bg-black w-1/2">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8">
-        <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+        <article>
+          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             Perchè lo faccio?
-        </h2>
-        <br>
+          </h2>
+        </article>
         <p class="flex Third-font max-w-2xl text-3xl mt-4 text-center sm:text-left text-orange-500 dark:text-white">
             <strong>95%</strong>
         </p>
@@ -16,7 +17,6 @@
             delle scelte di consumo sono prese in modo inconscio
           </p>
         </article>
-        <br>
         <p class="flex Third-font max-w-2xl text-3xl mt-4 text-center sm:text-left text-orange-500 dark:text-white">
             <strong>97%</strong>
           </p>
@@ -24,6 +24,8 @@
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             delle persone che sono consce della propria identità: vive più a lungo e meglio
           </p>
+        </article>
+        <article>
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
             il nostro inconscio prende una decisione <span class="Third-Font font-bold text-[#5bd086]">7</span> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
           </p>
