22317e3a1f4ef2a3fc54571460e75c1de85bc348
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-19 09:06:47 -0500

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -43,20 +43,17 @@ useHead({
                 Strategies for <span class="text-[#5bd086] dark:text-[#90FFB9]">individuality</span> and effective <span class="text-orange-300 dark:text-orange-300">identity</span> communication
                 </div>
               </div>
-              <button id="mobile-open-button" class="text-3xl focus:outline-none">
-                &#9776;
-              </button>
               <div class="topnav">
-  <a href="#home" class="active">Logo</a>
-  <div id="myLinks">
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-  </div>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
+                <a href="#home" class="active">Logo</a>
+                <div id="myLinks">
+                  <a href="#news">News</a>
+                  <a href="#contact">Contact</a>
+                  <a href="#about">About</a>
+                </div>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                  <i class="fa fa-bars"></i>
+                </a>
+              </div>
                 <nav id="Clicker" class="h-16 Second-Font hidden sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
                   <NuxtLink href="../services" class="link hover:opacity-90">
                   Services
