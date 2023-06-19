fcacb2fc9e508ed39684a8d68821cb6c31039b03
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-19 08:01:39 -0500

    Update Info1.vue

--- components/Info1.vue
+++ components/Info1.vue
@@ -2,49 +2,41 @@
 
 <template>
     <main class="max-4-4xl mx-auto">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
-        <h1 class="Third-font max-w-md text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
+        <h1 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             Who do I do it with?
           </h1>
       </section>
       <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
         <article>
           <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             With those approaching digital
           </h2>
-          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
-            <ul>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
                 With those who want to go online but don't know how to do it
-            </li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            <br>
                 With those who are already on social media but can't reach their goals
-            </li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            <br>>
                 With those who think these changes take up too much time and energy to work
-            </li>
-            </ul>
           </p>
         </article>
       </section>
       <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
         <article>
           <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             With those who want to do marketing
           </h2>
-          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
-            <ul>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">with those who would like to make a more structured or efficient marketing and communication plan</li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">with those who want to add a dash of innovation in their processes</li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">with those who want to develop their strategy themselves, but want support with small software tips, contacts with professionals for specific activities, or an outside perspective</li>
-            </ul>
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+              with those who would like to make a more structured or efficient marketing and communication plan<br>
+              with those who want to add a dash of innovation in their processes<br>
+              with those who want to develop their strategy themselves, but want support with small software tips, contacts with professionals for specific activities, or an outside perspective<br>
           </p>
         </article>
       </section>
       <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
         <article>
           <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             why call me for a project /<br>
@@ -55,12 +47,10 @@
           <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             With those who want to get the most out of
           </h2>
-          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
-            <ul>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">with those who would like to optimize the energy they are already investing in digital</li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">with those who would like to expand the social media they are already using</li>
-              <li class="Second-Font max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">with those who would like to improve the strategies they are already applying</li>
-            </ul>
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:text-left text-slate-700 dark:text-white">
+            with those who would like to optimize the energy they are already investing in digital<br>
+            with those who would like to expand the social media they are already using<br>
+            with those who would like to improve the strategies they are already applying<br>
           </p>
         </article>
       </section>
