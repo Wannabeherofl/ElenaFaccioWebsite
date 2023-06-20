e953844cebb3f7944602a237f4fccc666c8bfb58
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-20 10:17:20 -0500

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -24,7 +24,7 @@ useHead({
 </script>
 
   <template>
-      <header class="bg-white dark:bg-black text-black dark:text-white sticky top-0 z-10 flex flex-col-reverse justify-center items-center">
+      <header class="bg-white dark:bg-black text-black dark:text-white sm:sticky top-0 z-10 flex flex-col-reverse justify-center items-center">
         <div class="flex mx-40 pt-16 pb-2">
               <nav id="Clicker" class="active hidden h-16 Second-Font sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
               <NuxtLink class="link hover:opacity-90" to="/">
@@ -37,13 +37,13 @@ useHead({
                 </NuxtLink>
               </nav>
               <div>
-                <h1 class="Main-font text-7xl font-medium text-center pl-10 pr-10">Elena Faccio</h1>
+                <h1 class="Main-font sm:text-7xl text-2xl font-medium text-center pl-10 pr-10">Elena Faccio</h1>
                 <hr>
                 <div class="Second-Font text-center text-sm sm:text-2xl">
                 Strategies for <span class="text-[#5bd086] dark:text-[#90FFB9]">individuality</span> and effective <span class="text-orange-300 dark:text-orange-300">identity</span> communication
                 </div>
               </div>
-              <button id="mobile-open-button" class="block sm:hidden text-3xl focus:outline-none">
+              <button id="mobile-open-button" class="sticky block sm:hidden text-3xl focus:outline-none">
                 &#9776;
               </button>
                 <nav id="Clicker" class="h-16 Second-Font hidden sm:block space-x-8 text-2xl pr-6 pl-6" @click="menuTrigger()" aria-label="main">
