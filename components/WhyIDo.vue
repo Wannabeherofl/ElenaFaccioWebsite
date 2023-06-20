38398dc190d7e1faed032ec967b651eccf7998a0
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-20 09:31:42 -0500

    fix to VL

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -16,8 +16,6 @@
             delle scelte di consumo sono prese in modo inconscio
           </p>
         </article>
-      </section>
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8">
         <p class="flex Third-font max-w-2xl text-4xl mt-4 text-center sm:flex-row text-[#fdba74]">
             <strong>97%</strong>
           </p>
@@ -26,8 +24,6 @@
             delle persone che sono consce della propria identità: vive più a lungo e meglio
           </p>
         </article>
-      </section>
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8">
         <article>
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
             il nostro inconscio prende una decisione <span class="Third-Font font-bold text-[#5bd086]">7</span> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
