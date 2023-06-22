bec3df0cd077e78a5e8a10de0b8ebe1dee4e1d51
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-22 10:58:29 -0500

    Update Info2.vue

--- components/Info2.vue
+++ components/Info2.vue
@@ -1,53 +1,55 @@
 <script setup></script>
 
 <template>
-    <br>
-    <div class="text-center mb-12">
-        <div class="p-10 rounded-xl bg-[#121212] dark:bg-[#121212]">
-            <section>
-                <article class="Second-Font">
-                    <h1 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-                        How do I do it?
-                    </h1>
-                    <br>
-                    <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-                        Innovating    
-                    </h2>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                            Business Identity communication strategies
-                        <br>
-                            Business Strategies
-                        <br>
-                            Designing Digitization Pathways
-                        <br>
-                    </p>
-                    <br>
-                    <h2 class="Third-font text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                        teaching
-                    </h2>
-                    <p class="text-2xl mt-4 text-center text-slate-700 dark:text-white">
-                        <br>
-                            social/digital education 
-                       <br>
-                            Platform analysis
-                        <br>
-                            Provide guidelines for the use of key marketing tools
-                        <br>
-                        <h2 class="Third-font text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                            Marketing
-                        </h2>
-                            Marketing analysis and activities
-                        <br>
-                            Communication strategy development
-                        <br>
-                            Access to my network of professionals
-                        <br>
-                            Tailored back-office services
-                        <br>
-                            Project manager for site, logo and product renewal
-                    </p>
-                </article>
-            </section>
-        </div>
-    </div> 
+    <main class="max-4-4xl mx-auto">
+      <hr class="mx-auto bg-white dark:bg-black w-1/2 mb-12">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8">
+        <h2 class="Third-font max-w-2xl text-6xl font-bold text-center text-slate-900 dark:text-white">
+            How do I do it?
+          </h2>
+        <div class="vl"></div>
+        <article>
+            <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+                <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+                    Innovating    
+                </h2>
+                Business Identity communication strategies
+                <br>
+                Business Strategies
+                <br>
+                Designing Digitization Pathways
+                <br>
+            </p>
+          <hr>
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+                Teaching
+            </h2>
+            <br>
+            social/digital education 
+            <br>
+            Platform analysis
+            <br>
+            Provide guidelines for the use of key marketing tools
+            <br>    
+        </p>
+          <hr>
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
+                Marketing
+            </h2>
+            Marketing analysis and activities
+            <br>
+            Communication strategy development
+            <br>
+            Access to my network of professionals
+            <br>
+            Tailored back-office services
+            <br>
+            Project manager for site, logo and product renewal
+            </p>
+          <hr>
+        </article>
+      </section>
+    </main>
 </template>
