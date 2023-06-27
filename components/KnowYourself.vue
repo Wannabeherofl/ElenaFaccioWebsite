1941b746d524c65450271ee44d3db6ba7494d08f
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-27 10:36:41 -0500

    test

--- components/KnowYourself.vue
+++ components/KnowYourself.vue
@@ -9,9 +9,8 @@
         <h2 class="Third-font max-w-2xl text-6xl text-center text-slate-900 dark:text-white">
           skills direct behaviour for a known situation, but when you get into unknown situation, uncertain situation, we lean on our attributes. identity
           </h2>
-        <div class="vl"></div>
         <article>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white vl">
             Know your <strong class="text-[#fdba74]">identity</strong> means: you and your team make clear the skills for known situations, and know your attributes in unknow situation.<br>
             If you know your individuality you can comunicate it better, and build trust about your identity to others.<br>
             Trust is more than just a feeling, it is a belief. an feeling is just a human emotion justified, a belief is a human emotion justified and rationalized.
