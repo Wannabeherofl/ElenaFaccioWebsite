6d943580abb0a926321b386ca8302b9e1274e4ee
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-27 10:32:34 -0500

    Update WhyIDo.vue

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -4,10 +4,9 @@
     <main class="max-4-4xl mx-auto">
       <hr class="mx-auto bg-white dark:bg-black w-1/2 mb-12">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
-        <h2 class="Third-font max-w-2xl text-6xl text-center text-slate-900 dark:text-white">
+        <h2 class="Third-font max-w-2xl text-6xl text-center text-slate-900 dark:text-white vl">
             Perchè lo faccio?
           </h2>
-        <div class="vl h-1/2"></div>
         <article>
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
             <span class="Third-font text-[#5bd086]">95%</span> delle scelte di consumo sono prese in modo inconscio
