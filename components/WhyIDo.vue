21dbcea4847444842cc55b590f8a8ba491c7a9c7
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-27 10:15:58 -0500

    test

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -4,21 +4,21 @@
     <main class="max-4-4xl mx-auto">
       <hr class="mx-auto bg-white dark:bg-black w-1/2 mb-12">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
-        <h2 class="Third-font max-w-2xl text-6xl font-bold text-center text-slate-900 dark:text-white">
+        <h2 class="Third-font max-w-2xl text-6xl text-center text-slate-900 dark:text-white">
             Perchè lo faccio?
           </h2>
         <div class="vl"></div>
         <article>
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            <span class="Third-font font-bold text-[#5bd086]">95%</span> delle scelte di consumo sono prese in modo inconscio
+            <span class="Third-font text-[#5bd086]">95%</span> delle scelte di consumo sono prese in modo inconscio
           </p>
           <hr>
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            <span class="Third-font font-bold text-[#5bd086]">97%</span> delle persone che sono consce della propria <span class="Third-font font-bold text-[#fdba74]">identità</span>: vive più a lungo e meglio
+            <span class="Third-font text-[#5bd086]">97%</span> delle persone che sono consce della propria <span class="Third-font text-[#fdba74]">identità</span>: vive più a lungo e meglio
           </p>
           <hr>
           <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            il nostro inconscio prende una decisione <span class="Third-font font-bold text-[#5bd086]">7</span> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
+            il nostro inconscio prende una decisione <span class="Third-font text-[#5bd086]">7</span> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
           </p>
         </article>
       </section>
