97071f79520abd579001100aadc25c038e683650
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-27 12:38:10 -0500

    Update Identity.vue

--- components/Identity.vue
+++ components/Identity.vue
@@ -3,9 +3,9 @@
 </script>
 <template>
     <main class="max-4-4xl mx-auto">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12 vl rounded-xl">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
         <article>
-            <h2 class="Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
+            <h2 class="Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white rounded-xl vl">
                 Io credo che l'<strong class="text-[#fdba74]">identità</strong> professionale sia la base per ogni idea business 
             </h2>
         </article>
