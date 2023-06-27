4d9f877f5a22d2712321105c7682f98bb9d53ae6
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-27 12:34:18 -0500

    test

--- components/HomeInfo.vue
+++ components/HomeInfo.vue
@@ -55,13 +55,12 @@
                 145.74,256.51 150.07,253.49 161.00,244.00 Z" />
         </svg>
       </section>
-      <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
         <article>
           <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
             Cosa Faccio
           </h2>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          <p class="vl rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
             Aiuto le persone a rendere conscia la loro <strong class="text-[#fdba74]">identità</strong> professionale e più efficaci le loro attività di marketing e comunicazione
           </p>
         </article>
--- components/Identity.vue
+++ components/Identity.vue
@@ -3,8 +3,7 @@
 </script>
 <template>
     <main class="max-4-4xl mx-auto">
-      <hr class="mx-auto bg-white dark:bg-black w-1/2 mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
+      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12 vl rounded-xl">
         <article>
             <h2 class="Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
                 Io credo che l'<strong class="text-[#fdba74]">identità</strong> professionale sia la base per ogni idea business 
