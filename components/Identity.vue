9a4455fda297e9583950a9376459c0c13171f71a
Wannabeherofl <shilohnclemons@yahoo.com>
2023-06-27 13:23:16 -0500

    thin

--- components/Identity.vue
+++ components/Identity.vue
@@ -5,7 +5,7 @@
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
         <article>
-            <h2 class="vl2 Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
+            <h2 class="rounded-xl vl2 Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
                 Io credo che l'<strong class="text-[#fdba74]">identità</strong> professionale sia la base per ogni idea business 
             </h2>
         </article>
