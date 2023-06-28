b68f38e0f4e0a824710718e645efae59152317e2
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-28 10:44:15 -0500

    fix to vl

--- components/HomeInfo.vue
+++ components/HomeInfo.vue
@@ -8,7 +8,7 @@
           <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
             Cosa Faccio
           </h2>
-          <p class="vl2 rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          <p class="vl3 rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
             Aiuto le persone a rendere conscia la loro <strong class="text-[#fdba74]">identità</strong> professionale e più efficaci le loro attività di marketing e comunicazione
           </p>
         </article>
