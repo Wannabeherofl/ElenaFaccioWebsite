37167599e722b860575b380cd9ff8ad7c84f0ec1
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-28 10:58:16 -0500

    test

--- components/Info1.vue
+++ components/Info1.vue
@@ -2,55 +2,25 @@
 
 <template>
     <main class="max-4-4xl mx-auto">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
-        <h1 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-            Who do I do it with?
-          </h1>
-      </section>
-      <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
-        <article>
-          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-            With those approaching digital
+      <section id="hero" class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-40">
+        <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white mr-4">
+          With those approaching digital
           </h2>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        <article class="vl vl2 rounded-xl dark:bg-[#121212]">
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
             With those who want to go online but don't know how to do it
-            <br>With those who are already on social media but can't reach their goals
-            <br>With those who think these changes take up too much time and energy to work
           </p>
-        </article>
-      </section>
-      <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
-        <article>
-          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-            With those who want to do marketing
-          </h2>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-              with those who would like to make a more structured or efficient marketing and communication plan<br>
-              with those who want to add a dash of innovation in their processes<br>
-              with those who want to develop their strategy themselves, but want support with small software tips, contacts with professionals for specific activities, or an outside perspective<br>
+          <hr>
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            With those who are already on social media but can't reach their goals
           </p>
-        </article>
-      </section>
-      <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-12">
-        <article>
-          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-            why call me for a project /<br>
-            why make me your consultant /<br> 
-            how can i help you with /<br> 
-            which goal i can help you with<br>
-          </h2>
-          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-            With those who want to get the most out of
-          </h2>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            with those who would like to optimize the energy they are already investing in digital<br>
-            with those who would like to expand the social media they are already using<br>
-            with those who would like to improve the strategies they are already applying<br>
+          <hr>
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            With those who think these changes take up too much time and energy to work
           </p>
         </article>
       </section>
     </main>
-  </template>
+</template>
+
+
--- components/Info2.vue
+++ components/Info2.vue
@@ -2,54 +2,72 @@
 
 <template>
     <main class="max-4-4xl mx-auto">
-      <hr class="mx-auto bg-white dark:bg-black w-1/2 mb-12">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8">
-        <h2 class="Third-font max-w-2xl text-6xl font-bold text-center text-slate-900 dark:text-white">
-            How do I do it?
-          </h2>
-        <div class="vl"></div>
+      <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-40">
         <article>
-            <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-                    Innovating    
+            <h2 class="rounded-xl vl3 Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
+              How do I do it?
             </h2>
-            <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-                Business Identity communication strategies
-                <br>
-                Business Strategies
-                <br>
-                Designing Digitization Pathways
-                <br>
-            </p>
-            <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-                Teaching
-            </h2>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            <br>
-            social/digital education 
-            <br>
-            Platform analysis
-            <br>
-            Provide guidelines for the use of key marketing tools
-            <br>    
-        </p>
-        <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
-          <h2 class="Third-font max-w-2xl text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
-                Marketing
-            </h2>
-          <p class="Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            Marketing analysis and activities
-            <br>
-            Communication strategy development
-            <br>
-            Access to my network of professionals
-            <br>
-            Tailored back-office services
-            <br>
-            Project manager for site, logo and product renewal
-            </p>
-            <hr class="mx-auto bg-white dark:bg-black w-1/2  mb-12">
         </article>
       </section>
-    </main>
+      <section id="hero" class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-40">
+        <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white mr-4">
+          Innovating
+          </h2>
+        <article class="vl vl2 rounded-xl dark:bg-[#121212]">
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            Business Identity communication strategies
+          </p>
+          <hr>
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            Business Strategies
+          </p>
+          <hr>
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            Designing Digitization Pathways
+          </p>
+        </article>
+      </section>
+      <section id="hero" class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-40">
+    <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white mr-4">
+      Teaching
+      </h2>
+    <article class="vl vl2 rounded-xl dark:bg-[#121212]">
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        social/digital education
+      </p>
+      <hr>
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Platform analysis
+      </p>
+      <hr>
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Provide guidelines for the use of key marketing tools
+      </p>
+    </article>
+  </section>
+  <section id="hero" class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-40">
+    <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white mr-4">
+      Marketing
+      </h2>
+    <article class="vl vl2 rounded-xl dark:bg-[#121212]">
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Marketing analysis and activities
+      </p>
+      <hr>
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Communication strategy development
+      </p>
+      <hr>
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Access to my network of professionals
+      </p>
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Tailored back-office services
+      </p>
+      <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+        Project manager for site, logo and product renewal
+      </p>
+    </article>
+  </section>
+  </main>
 </template>
