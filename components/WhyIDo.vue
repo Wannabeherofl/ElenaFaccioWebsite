695fbd679175f2a0c601955fad50ac5833766ad7
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-28 08:09:14 -0500

    app

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -2,10 +2,10 @@
 
 <template>
     <main class="max-4-4xl mx-auto">
-      <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center mb-40">
-        <h2 class="Third-font max-w-2xl text-6xl text-center text-slate-900 dark:text-white mr-4">
+      <section id="hero" class="hero flex justify-center sm:flex-row p-6 items-center mb-40">
+        <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white mr-4">
             Perchè lo faccio?
-          </h2>
+        </h2>
         <article class="vl rounded-xl dark:bg-[#121212]">
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
             <span class="Third-font text-[#90FFB9]">95%</span> delle scelte di consumo sono prese in modo inconscio
