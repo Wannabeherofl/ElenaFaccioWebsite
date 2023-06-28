ede7e549b16f006e0d6648366f80ea07a534fbf6
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-06-28 09:25:51 -0500

    test

--- components/HeroPage.vue
+++ components/HeroPage.vue
@@ -8,23 +8,23 @@
           Wonderful Clients
         </h2>
         <ul class="list-none mx-auto mt-12 flex flex-row items-center sm:gap-8 gap-4">
-          <li class="w-24 sm:w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-[#121212] py-1 px-1 rounded">
+          <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-[#121212] py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/boxcamilla.jpg" alt="Camilla">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Camilla</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">Cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-white">Cat</p>
+            <h3 class="Second-Font sm:text-3xl text-center text-slate-900 dark:text-white">Camilla</h3>
+            <p class="Second-Font hidden sm:block text-base sm:text-3xl text-center mt-2 text-slate-400">Cat</p>
+            <p class="Second-Font sm:hidden sm:text-2xl text-center mt-2 text-slate-500 dark:text-white">Cat</p>
           </li>
-          <li class="w-24 sm:w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-[#121212] py-1 px-1 rounded">
+          <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-[#121212] py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/boxzelda.jpg" alt="Zelda">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Zelda</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">also a cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-white">also a cat</p>
+            <h3 class="Second-Font sm:text-3xl text-center text-slate-900 dark:text-white">Zelda</h3>
+            <p class="Second-Font hidden sm:block text-base sm:text-3xl text-center mt-2 text-slate-400">also a cat</p>
+            <p class="Second-Font sm:hidden sm:text-2xl text-center mt-2 text-slate-500 dark:text-white">also a cat</p>
           </li>
-          <li class="w-24 sm:w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-[#121212] py-1 px-1 rounded">
+          <li class="w-1/2 flex flex-col items-center border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-[#121212] py-1 px-1 rounded">
             <img class="mb-6 rounded" src="../assets/boxElena.jpg" alt="Camilla">
-            <h3 class="Second-Font text-3xl text-center text-slate-900 dark:text-white">Me</h3>
-            <p class="Second-Font hidden sm:block text-3xl text-center mt-2 text-slate-400">possibly a cat</p>
-            <p class="Second-Font sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-white">possibly a cat</p>
+            <h3 class="Second-Font sm:text-3xl text-center text-slate-900 dark:text-white">Me</h3>
+            <p class="Second-Font hidden sm:block text-base sm:text-3xl text-center mt-2 text-slate-400">possibly a cat</p>
+            <p class="Second-Font sm:hidden sm:text-2xl text-center mt-2 text-slate-500 dark:text-white">possibly a cat</p>
           </li>
         </ul>
         <div class="pt-5 flex items-center text-center gap-8 Second-Font text-2xl text-slate-700 dark:text-white">
