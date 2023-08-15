c3be59bab5723e2f4a2bbb1ff8efe7c3cd467c94
Wannabeherofl <shilohnclemons@yahoo.com>
2023-08-15 13:07:31 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,10 @@ export default defineNuxtConfig({
           { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
           { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=PT+Serif:wght@400;700&display=swap' },
         ],
-        script: [{ src: 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' }],
+        script: [
+          { src: 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' },
+        { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' }
+      ],
       },
     },
     css: ['~/assets/css/main.css'],
