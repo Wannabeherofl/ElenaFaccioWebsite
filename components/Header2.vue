2a1f6bb477df63619950975e23000d80aa73ae65
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-08-15 10:15:42 -0500

    Update Header2.vue

--- components/Header2.vue
+++ components/Header2.vue
@@ -5,17 +5,17 @@
 <header>
     <nav class="align-center dropdown md:container-fluid mx-auto md:p-4 pt-4 px-4 md:flex justify-between overflow-auto row bg-white dark:bg-black text-black dark:text-white">
         <ul class="md:flex invisible md:visible gap-4 px-auto">
-            <NuxtLink to="/" class="Second-font text-2xl">Home</NuxtLink>
-            <NuxtLink to="/about" class="Second-font text-2xl">About</NuxtLink>
+            <NuxtLink to="/" class="Second-Font text-2xl">Home</NuxtLink>
+            <NuxtLink to="/about" class="Second-Font text-2xl">About</NuxtLink>
             <div class="static mx-auto">
                 <h1 class="Main-font sm:text-7xl text-6xl font-medium text-center pl-10 pr-10">Elena Faccio</h1>
                 <hr>
-                <div class="Second-Font text-center text-sm sm:text-2xl">
+                <div class="Second-font text-center text-sm sm:text-2xl">
                 Strategies for <span class="text-[#7affab] dark:text-[#90FFB9]">individuality</span> and effective <span class="text-orange-300 dark:text-orange-300">identity</span> communication
                 </div>
             </div>
-            <NuxtLink to="/contact" class="Second-font text-2xl">Contact</NuxtLink>
-            <NuxtLink to="/contact" class="Second-font text-2xl">Services</NuxtLink>
+            <NuxtLink to="/contact" class="Second-Font text-2xl">Contact</NuxtLink>
+            <NuxtLink to="/contact" class="Second-Font text-2xl">Services</NuxtLink>
         </ul>
     </nav>
 </header>
