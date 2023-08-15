ca28a2308aec1b2baa4bd403fc2f334a85b0be51
Wannabeherofl <shilohnclemons@yahoo.com>
2023-08-15 12:43:39 -0500

    language

--- components/HomeInfo.vue
+++ components/HomeInfo.vue
@@ -5,10 +5,16 @@
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-40">
         <article>
-          <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
+          <h2 lang="it" class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
             Cosa Faccio
           </h2>
-          <p class="vl3 rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          <h2 lang="en" class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
+            What I do
+          </h2>
+          <p lang="en" class="vl3 rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            AI help people make their business <strong class="text-[#fdba74]">individuality</strong> conscious and their marketing and communication activities more effective
+          </p>
+          <p lang="it" class="vl3 rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
             Aiuto le persone a rendere conscia la loro <strong class="text-[#fdba74]">identità</strong> professionale e più efficaci le loro attività di marketing e comunicazione
           </p>
         </article>
