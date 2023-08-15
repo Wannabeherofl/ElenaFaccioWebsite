7a6bd44b1f9b3c597027ab40922191987a5312fd
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-08-15 11:18:56 -0500

    header update

--- components/Header2.vue
+++ components/Header2.vue
@@ -7,15 +7,15 @@
         <div class="md:flex invisible md:visible gap-4 px-auto">
             <NuxtLink to="/" class="Second-Font text-2xl ml-auto static">Home</NuxtLink>
             <NuxtLink to="../AboutMe" class="Second-Font text-2xl static">About Me</NuxtLink>
-            <div class="static mx-auto">
+            <div class="static">
                 <h1 class="Main-font sm:text-7xl text-6xl font-medium text-center pl-10 pr-10">Elena Faccio</h1>
                 <hr>
                 <div class="Second-font text-center text-sm sm:text-2xl">
                 Strategies for <span class="text-[#7affab] dark:text-[#90FFB9]">individuality</span> and effective <span class="text-orange-300 dark:text-orange-300">identity</span> communication
                 </div>
             </div>
-            <NuxtLink to="/contact" class="Second-Font text-2xl  static">Contact</NuxtLink>
-            <NuxtLink to="/contact" class="Second-Font text-2xl mr-auto static">Services</NuxtLink>
+            <NuxtLink to="../Contact" class="Second-Font text-2xl  static">Contact</NuxtLink>
+            <NuxtLink to="../Services" class="Second-Font text-2xl mr-auto static">Services</NuxtLink>
         </div>
     </nav>
 </header>
