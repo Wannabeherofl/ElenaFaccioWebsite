42cae6ac1beb06f6486f85e5e3bbdad8b1109b32
Wannabeherofl <shilohnclemons@yahoo.com>
2023-08-15 12:48:24 -0500

    test

--- components/Header2.vue
+++ components/Header2.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
 <header class="bg-white dark:bg-black text-black dark:text-white sm:sticky top-0 z-10 flex flex-col-reverse justify-center items-center">
-    <nav class="align-center dropdown sticky mx-auto pt-4 px-4 justify-between row bg-white dark:bg-black text-black dark:text-white">
+    <nav class="align-center dropdown sticky mx-auto pt-4 pb-4 px-4 justify-between row bg-white dark:bg-black text-black dark:text-white">
         <div class="md:flex invisible md:visible gap-8 px-auto sticky">
             <NuxtLink to="/" class="pt-8 Second-Font text-2xl ml-auto static">Home</NuxtLink>
             <NuxtLink to="../AboutMe" class="pt-8 Second-Font text-2xl static">About Me</NuxtLink>
