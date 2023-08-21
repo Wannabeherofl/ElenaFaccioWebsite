fef4242b625596ee1da10b2c55a66296938443fe
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-08-21 10:08:18 -0500

    Update Identity.vue

--- components/Identity.vue
+++ components/Identity.vue
@@ -5,7 +5,7 @@
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-40">
         <article>
-            <h2 class="rounded-xl vl3 Third-font max-w-2xl text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
+            <h2 class="rounded-xl vl3 Third-font px-30 text-3xl text-center sm:text-5xl text-slate-900 dark:text-white">
               Pprofessional identity is the basis for any business model.
 Comunicationg it better it means trusting you are comunicationg it most coherent and true way.
 Trust is more than just a feeling, it is a belief. an feeling is just a human emotion justified, a belief is a human emotion justified and rationalized.
