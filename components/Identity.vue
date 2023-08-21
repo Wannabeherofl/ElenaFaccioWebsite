2a8ad188cbbf88c3c01311c8f0801da7781fb01a
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-08-21 11:40:54 -0500

    Update Identity.vue

--- components/Identity.vue
+++ components/Identity.vue
@@ -6,11 +6,11 @@
       <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-40">
         <article>
             <h2 class="rounded-xl vl3 Third-font mx-60 text-3xl text-center text-slate-900 dark:text-white">
-              Professional identity is the basis for any business model.
-Comunicationg it better it means trusting you are comunicationg it most coherent and true way.
-Trust is more than just a feeling, it is a belief. an feeling is just a human emotion justified, a belief is a human emotion justified and rationalized.
-
-and that what we will do, understand and rationalize your identity</h2>
+              Professional identity is the basis for any business model.<br>
+              Comunicationg it better it means trusting you are comunicationg it most coherent and true way.<br>
+              Trust is more than just a feeling, it is a belief. <br>
+              A feeling is just a human emotion justified, a belief is a human emotion justified and rationalized.<br>
+              and that what we will do, understand and rationalize your identity</h2>
         </article>
       </section>
     </main>
