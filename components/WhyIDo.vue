668a233a987f6e78baca95130b9b88422dd9ed4a
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-08-21 10:32:05 -0500

    Update WhyIDo.vue

--- components/WhyIDo.vue
+++ components/WhyIDo.vue
@@ -4,19 +4,19 @@
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-40">
         <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white mr-4">
-            Perchè lo faccio?
+            Why do I do it?
         </h2>
         <article class="vl vl2 rounded-xl dark:bg-[#121212]">
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            <span class="Third-font text-[#90FFB9]">95%</span> delle scelte di consumo sono prese in modo inconscio
+            <span class="Third-font text-[#90FFB9]">95%</span>Of consumer choices are taken subconsciously
           </p>
           <hr>
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            <span class="Third-font text-[#90FFB9]">97%</span> delle persone che sono consce della propria <span class="Third-font text-[#fdba74]">identità</span>: vive più a lungo e meglio
+            <span class="Third-font text-[#90FFB9]">97%</span> of people who are aware of their own <span class="Third-font text-[#fdba74]">identity</span>:live longer and better
           </p>
           <hr>
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            il nostro inconscio prende una decisione <span class="Third-font text-[#90FFB9]">7</span> secondi prima di quando le formuliamo consciamente e circa dieci secondi prima di eseguirle
+            Your unconscious makes a decision  <span class="Third-font text-[#90FFB9]">7</span> seconds before we consciously formulate them and about ten seconds before we execute them.
           </p>
         </article>
       </section>
