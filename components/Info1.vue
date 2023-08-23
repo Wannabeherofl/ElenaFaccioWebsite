040ee30695c0d1e78f6c67b37af0f37efd9304d6
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2023-08-23 10:45:15 -0500

    Update Info1.vue

--- components/Info1.vue
+++ components/Info1.vue
@@ -4,6 +4,7 @@
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-4">
         <article>
+          <div class="max-w-2xl vl3"></div>
             <h2 class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
               Who do I do it with?
             </h2>
@@ -11,9 +12,7 @@
       </section>
       <section  class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-4">
         <article>
-          <div class="max-w-2xl vl3">
             <img src="../assets\Pexels2.jpg" class="flex rounded-full border-6 border-orange-300 mb-6 w-72">
-          </div>
         </article>
       </section>
       <section class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-20">
