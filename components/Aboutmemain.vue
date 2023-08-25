c9632e76a5e62abfba56c8dce4dcf657a54e08a3
Wannabeherofl <shilohnclemons@yahoo.com>
2023-08-25 08:32:04 -0500

    Update Aboutmemain.vue

--- components/Aboutmemain.vue
+++ components/Aboutmemain.vue
@@ -12,7 +12,7 @@
           <h2 class="Third-font max-w-md text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
             Elena Faccio 
           </h2>
-          <p class="Second-Font max-w-2xl text-2xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-2xl text-2xl mt-4 sm:flex-row text-slate-700 dark:text-white">
             Hi! My name is Elena and I am a Marketing and strategy consultant. I am passionate about individuality and enjoy working on project with people.<br>
 My passion about marketing and comunication started at very early age, I remember me at <span class="text-bold Third-font text-[#fdba74]">6</span> wondering about what is it and where are the roots for it. While studiend and researching about identity I started my volunteer carrer at <span class="text-bold Third-font text-[#fdba74]">12</span> and that made me understand that what i want to do in life is understand what people want need and help them to get it. that is why i chosed the University degree in Marketing Management.
 After that I continued studing attending a class about tourism field focused on territory comunication and tourist welcoming.
@@ -24,10 +24,10 @@ In <span class="Third-font text-[#fdba74] text-bold">2023</span> I ejoyed the on
       <section id="hero" class="hero flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12  ml-60">
         <img src="../assets/Silly.png" class="flex rounded-full border-6 border-orange-300 mb-6 h-72">
         <article class="sm:w-1/2">
-          <h2 class="Third-font max-w-auto text-4xl font-bold text-center sm:text-5xl sm:flex-right text-slate-900 dark:text-white">
+          <h2 class="Third-font max-w-auto text-4xl font-bold text-center sm:text-5xl text-slate-900 dark:text-white">
             Hobbies
           </h2>
-          <p class="Second-Font max-w-md text-2xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          <p class="Second-Font max-w-md text-2xl mt-4 sm:flex-row text-slate-700 dark:text-white">
             In my free time i enjoy do my hobbies, which are many and very diverse.<br>
 I am a passionate book lover, I enjoy Aikido and I regularly practice meditation, while also enjoying playing JRPG video games in my free time. 
 <br>
