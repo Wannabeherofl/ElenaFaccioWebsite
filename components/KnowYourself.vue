0c637054c444a92ea5d15c83c45dd7b2653fd02a
Wannabeherofl <shilohnclemons@yahoo.com>
2023-08-25 14:34:54 -0500

    Update KnowYourself.vue

--- components/KnowYourself.vue
+++ components/KnowYourself.vue
@@ -4,17 +4,21 @@
 </script>
 <template>
     <main class="max-4-4xl mx-auto">
+      <section id="hero" class="hero flex flex-col justify-center sm:flex-row p-6 items-center mb-8">
+      <article>
+        <h2 language="English" class="Third-font max-w-2xl text-4xl text-center sm:text-5xl text-slate-900 dark:text-white">
+        </h2>
+        <p language="English" class="vl3 rounded-xl Second-Font max-w-2xl text-3xl mt-4 text-center sm:flex-row text-slate-700 dark:text-white">
+          Skills direct behaviour for a known situation, but when you get into unknown situation, uncertain situation, we lean on our attributes. identity
+        </p>
+      </article>
+    </section>
       <section id="hero" class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-8">
         <img src="../assets\pexels-john-diez-7578192.jpg" class="mr-20 flex rounded-full border-6 border-orange-300 mb-6 w-72 h-72">
         <article class="vl vl2 rounded-xl dark:bg-[#121212]">
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
-            Skills direct behaviour for a known situation, but when you get into unknown situation, uncertain situation, we lean on our attributes. identity
-          </p>
-          <hr>
-          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
             Know your <strong class="text-[#fdba74]">identity</strong> means: you and your team make clear the skills for known situations, and know your attributes in unknow situation.
           </p>
-          <hr>
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
             If you know your individuality you can comunicate it better, and build trust about your identity to others.
           </p>
