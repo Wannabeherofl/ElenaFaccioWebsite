3ef97c20fe04a91e6709fa82d14fed6b23c55789
Wannabeherofl <shilohnclemons@yahoo.com>
2023-08-25 08:18:28 -0500

    Update KnowYourself.vue

--- components/KnowYourself.vue
+++ components/KnowYourself.vue
@@ -5,6 +5,14 @@
 <template>
     <main class="max-4-4xl mx-auto">
       <section id="hero" class="hero flex justify-center flex-col sm:flex-row p-6 items-center mb-8">
+        <article class="vl vl2 rounded-xl dark:bg-[#121212]">
+          <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
+            Professional identity is the basis for any business model.
+            <br>Comunicationg it better it means trusting you are comunicationg it most coherent and true way.
+            <br>Trust is more than just a feeling, it is a belief. an feeling is just a human emotion justified, a belief is a human emotion justified and rationalized.
+            <br>And that what we will do, understand and rationalize your identity.
+          </p>
+        </article>
         <img src="../assets\pexels-john-diez-7578192.jpg" class="mr-20 flex rounded-full border-6 border-orange-300 mb-6 w-72 h-72">
         <article class="vl vl2 rounded-xl dark:bg-[#121212]">
           <p class="Second-Font max-w-2xl text-3xl my-4 text-center sm:flex-row text-slate-700 dark:text-white">
